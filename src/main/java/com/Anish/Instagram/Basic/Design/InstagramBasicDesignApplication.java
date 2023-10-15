2f27716e825370a8d37c4130244c2a477644d506
Anish Kumar Sharma <anish.kumar20071998@gmail.com>
2023-10-15 12:36:14 +0530

    "Project Starting"

---
+++ src/main/java/com/Anish/Instagram/Basic/Design/InstagramBasicDesignApplication.java
@@ -0,0 +1,14 @@
+package com.Anish.Instagram.Basic.Design;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagramBasicDesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstagramBasicDesignApplication.class, args);
+		System.out.println("Anish is Creating a project");
+	}
+
+}
